24f2d1ca5e25f62bccf0e7427589852234a099c3
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-06 15:15:15 -0300

    Paquete Yargs

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 
-//RECIBIR INFORMACIÓN DESDE LÍNEA DE COMANDO
+//PAQUETE YARGS
 
-//Hay varios objetos que están corriendo en Node al momento en que la aplicación se ejecuta.
+//Este es un paquete:
+const argv = require('yargs')
+            .command('listar', 'Imprime en consola la tabla de multiplicar', {
+              base: {//comando que el usuario puede escribir
+                  demand: true, //valor obligatorio
+                  alias: 'b' //Esto es para los shorcuts, en lugar de --base ingresa -b
+              },
+              limite: {
+                alias: 'l',
+                default: 10//En caso que el usuario no ingrese ningún límite.
+              }
+            })//El segundo argumento es una ayuda para mostrar al usuario. El tercer argumento es un objeto que va a recibir la configuración de parámetros o de slash (--, -) que este comando puede recibir
+            .help()//Imprime una ayuda con el comando --help (node app listar --help o node app --help). Esto lo crea yargs.
+            .argv;
+
+//Ingresar por consola: node app listar --base para ver la base
 
 const {crearArchivo} = require('./multiplicar/multiplicar.js')
+//Este es un path relativo por eso tiene ./ etc.
 
-//console.log(process);//Cuando Node se ejecuta crea una variable de entorno llamada process. Es un objeto igual que module.
-//console.log(process.argv);//Imprime la ubicación de node y la ubicación del archivo que estamos utilizando en este momento. Con el comando node app --base=7, le puedo mandar otro parámetro: base=7 de tipo string.
 
-let argv = process.argv;//Esto lo hacemos para que sea más sencillo. Ponemos process.argv dentro de una variable.
-let parametro = argv[2];//Selecciono la base que le paso como tercer parámetro
-let base = parametro.split('=')[1]//El 1 porque quiero el segundo elemento, que es el número.
-//parametro es un string. split es parar separar elementos, para convertir de un string a un arreglo. Lo voy a separar por el signo = y así me quedo solo con el número.
-//console.log(base);//prueba para ver si funciona
+let argv2 = process.argv;
+console.log(argv.base);
+console.log('Limite', argv.limite);
+//console.log(argv2);
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
 
-crearArchivo(base)
-  .then(archivo => console.log(`Archivo creado ${archivo}`))
-  .catch(e => console.log(e));
+// crearArchivo(base)
+//   .then(archivo => console.log(`Archivo creado ${archivo}`))
+//   .catch(e => console.log(e));
 
-  //Estamos ejecutando nuestra aplicación de Node recibiendo parámetros que son enviados desde la terminal directamente con node app --base=4.
-
-  //Igual es bastante difícil capturar la base así y necesita mucha más validaciones compo por ejemplo, si no se introduce ninguna base... etc. Si no lo mando como primer parámetro también es un problema. Porque toma lo que sea que está en primer lugar como la base. Por eso ya hay paquetes creados parar solucionar todo esto.
+  
