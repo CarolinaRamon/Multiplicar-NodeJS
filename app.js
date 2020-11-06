7c57fbb469d2e9c65f6f1ab5f323a485e5ca4d5e
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-06 15:54:55 -0300

    Optimizaciones para la configuración del Yargs

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 
-//EJECUTAR EL COMANDO LISTAR
+//OPTIMIZACIONES PARA LA CONFIGURACIÓN DEL YARGS
 
 
-const argv = require('yargs')
-            .command('listar', 'Imprime en consola la tabla de multiplicar', {
-              base: {
-                  demand: true, 
-                  alias: 'b'
-              },
-              limite: {
-                alias: 'l',
-                default: 10
-              }
-            })
-            .command('crear', 'Genera un archivo con la tabla de multiplicar', {
-              base: {
-                  demand: true, 
-                  alias: 'b'
-              },
-              limite: {
-                alias: 'l',
-                default: 10
-              }
-            })
-            .help()
-            .argv;
-
+const argv = require('./config/yargs').argv;//Podría también hacer argv.argv, es decir, traigo todo el require, lo guardo en argv y dentro del argv hay otro argv al que accedo con el .argv. Pero es mejor extraer solamente lo que necesito y  guardarlo directamente en la const.
+        
 //Para listar, ingresar por consola: node app listar --limite 30 --base 6, por ejemplo
 //Para crear, ingresar por consola: node app crear --limite 5 --base , por ejemplo
 
