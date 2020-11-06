fac67d5371f554d090653933e012f5fd33bdbd81
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-06 15:09:35 -0300

    Recibir información desde línea de comando

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 
-//IMPORTAR ARCHIVOS DE NUESTRO PROYECTO
+//RECIBIR INFORMACIÓN DESDE LÍNEA DE COMANDO
 
+//Hay varios objetos que están corriendo en Node al momento en que la aplicación se ejecuta.
 
-//const multiplicar = require('./multiplicar/multiplicar.js')//La extensión js en un require es opcional porque es redundante, ya que se sabe que tiene que ser un archivo js
+const {crearArchivo} = require('./multiplicar/multiplicar.js')
 
-//console.log(multiplicar); //multiplicar tiene una propiedad que se llama crearArchivo que es una función
-//multiplicar.crearArchivo //podría acceder así, pero es mejor simplemente llamar a la función. Eso lo podemos hacer gracias a la destructuración.
+//console.log(process);//Cuando Node se ejecuta crea una variable de entorno llamada process. Es un objeto igual que module.
+//console.log(process.argv);//Imprime la ubicación de node y la ubicación del archivo que estamos utilizando en este momento. Con el comando node app --base=7, le puedo mandar otro parámetro: base=7 de tipo string.
 
-const {crearArchivo} = require('./multiplicar/multiplicar.js')//Si después de una const siguen llaves {}, quiere decir que es una destructuración.
-
-//Tablas de multiplicar:
-
-// let base = 'abc';//prueba
-let base = 7;
-
+let argv = process.argv;//Esto lo hacemos para que sea más sencillo. Ponemos process.argv dentro de una variable.
+let parametro = argv[2];//Selecciono la base que le paso como tercer parámetro
+let base = parametro.split('=')[1]//El 1 porque quiero el segundo elemento, que es el número.
+//parametro es un string. split es parar separar elementos, para convertir de un string a un arreglo. Lo voy a separar por el signo = y así me quedo solo con el número.
+//console.log(base);//prueba para ver si funciona
 
 crearArchivo(base)
   .then(archivo => console.log(`Archivo creado ${archivo}`))
   .catch(e => console.log(e));
 
-// console.log(module);
+  //Estamos ejecutando nuestra aplicación de Node recibiendo parámetros que son enviados desde la terminal directamente con node app --base=4.
+
+  //Igual es bastante difícil capturar la base así y necesita mucha más validaciones compo por ejemplo, si no se introduce ninguna base... etc. Si no lo mando como primer parámetro también es un problema. Porque toma lo que sea que está en primer lugar como la base. Por eso ya hay paquetes creados parar solucionar todo esto.
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -3,9 +3,8 @@
 const fs = require('fs');
 
 let crearArchivo = (base) => {
-//module.exports.crearArchivo = (base) => {//Esto es otra forma de hacer lo del module. Y sin el let.
 
-    return new Promise ((resolve, reject) => {//retorno una promesa
+    return new Promise ((resolve, reject) => {
 
         if(!Number(base)){
             reject(`El valor introducido ${base} no es un número.`);
@@ -22,22 +21,14 @@ let crearArchivo = (base) => {
          if (err) 
             reject(err)
          else
-            resolve(`tabla-${base}.txt`)//El resolve está esperando el nombre del archivo
+            resolve(`tabla-${base}.txt`)
     });
 
     });
 }
 
-
-//module es un objeto global que está disponible a lo largo de toda la aplicación y que incluye una propiedad que se llama exports y sirve para exportar objetos.
-//Acá estoy agregando a exports lo que quiero exportar.
 module.exports = {
-    //crearArchivo: crearArchivo //En ECMAScript6 no es necesario escribirlo así cuando el valor es igual a la propiedad.
     crearArchivo
-    // crearArchivo,
-    // crearArchivo,
-    // crearArchivo,
-    //Esta forma es mejor que agregarlo arriba porque si tuvieramos muchas funciones simplemente las podríamos agregar aquí una debajo de la otra.
 }
 
 
